7644a15aeac99c6d252da63f8ccfe5628e688dd3
Daniel Hladík <daniel@hladik.dev>
2022-04-25 11:17:53 +0200

    updated

--- macos-fix-icons.sh
+++ macos-fix-icons.sh
@@ -5,11 +5,11 @@
 # $3 - Optional - Custom path
 fix_icon()
 {
-    path_current_icon="${${3#0}:-"/Applications"}/${1}.app/Contents/Resources/${2}.icns"
-    path_new_icon="${0:A:h}/icons/${${1:l}// /_}.icns"
+    path_current_icon=$(compgen -G "${${3#0}:-"/Applications"}/${1}.app/Contents/Resources/${2}.icns")
+    path_new_icon="${0:A:h}/icons/${${${1:l}// /_}//\*/}.icns"
 
     if [[ ! -f ${path_current_icon} ]] then
-        echo "${1} - Skipped - Does not exist"
+        # echo "${1//\*/} - Skipped - Does not exist"
         return
     fi
 
@@ -17,16 +17,21 @@ fix_icon()
     shasum_new_icon=$(shasum --algorithm 256 ${path_new_icon} | cut -d " " -f 1 )
 
     if [[ ${shasum_current_icon} = ${shasum_new_icon} ]] then
-        echo "${1} - Skipped - Already replaced"
+        echo "${1//\*/} - Skipped - Already replaced"
         return
     fi
 
     cp "${path_new_icon}" "${path_current_icon}"
-    touch "/Applications/${1}.app"
+    touch "/Applications/${1//\*/}.app"
 
-    echo "${1} - Replaced"
+    echo "${1//\*/} - Replaced"
 }
 
+autoload -Uz compinit && compinit
+autoload -Uz bashcompinit && bashcompinit
+
+fix_icon "HandBrake" "HandBrake"
+fix_icon "MKVToolNix*" "MKVToolNix"
 fix_icon "Opera" "app"
 fix_icon "Plex" "Desktop"
 fix_icon "Spotify" "Icon"
